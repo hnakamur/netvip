647de448b004f03d9c9425596b1bf301699d6d38
Hiroaki Nakamura <hnakamur@gmail.com>
2022-09-25 03:46:30 +0900

    Rename macAddrBroadcast

--- garp.go
+++ garp.go
@@ -7,7 +7,7 @@ import (
 	"github.com/mdlayher/arp"
 )
 
-var macBroadcast = net.HardwareAddr{0xff, 0xff, 0xff, 0xff, 0xff, 0xff}
+var macAddrBroadcast = net.HardwareAddr{0xff, 0xff, 0xff, 0xff, 0xff, 0xff}
 
 // SendGARP sends a GARP (Gratuitous ARP) packet.
 // See https://wiki.wireshark.org/Gratuitous_ARP for GARP.
@@ -19,11 +19,11 @@ func SendGARP(intf *net.Interface, addr netip.Addr) error {
 	defer c.Close()
 
 	p, err := arp.NewPacket(arp.OperationRequest, intf.HardwareAddr, addr,
-		macBroadcast, addr)
+		macAddrBroadcast, addr)
 	if err != nil {
 		return err
 	}
-	err = c.WriteTo(p, macBroadcast)
+	err = c.WriteTo(p, macAddrBroadcast)
 	if err != nil {
 		return err
 	}
