781a3c3647a234313ea17f8708265461d7aea0b5
Hiroaki Nakamura <hnakamur@gmail.com>
2022-11-13 03:19:47 +0900

    Unexport parseARPPacket and isGARPPacket

--- garp.go
+++ garp.go
@@ -35,37 +35,6 @@ func SendGARP(intf *net.Interface, addr netip.Addr) error {
 	return nil
 }
 
-var (
-	// errInvalidARPPacket is returned when an ethernet frame does not
-	// indicate that an ARP packet is contained in its payload.
-	errInvalidARPPacket = errors.New("invalid ARP packet")
-)
-
-func ParseARPPacket(buf []byte) (*arp.Packet, *ethernet.Frame, error) {
-	f := new(ethernet.Frame)
-	if err := f.UnmarshalBinary(buf); err != nil {
-		return nil, nil, err
-	}
-
-	// Ignore frames which do not have ARP EtherType
-	if f.EtherType != ethernet.EtherTypeARP {
-		return nil, nil, errInvalidARPPacket
-	}
-
-	p := new(arp.Packet)
-	if err := p.UnmarshalBinary(f.Payload); err != nil {
-		return nil, nil, err
-	}
-	return p, f, nil
-}
-
-func IsGARPPacket(p *arp.Packet, vip netip.Addr) bool {
-	return p.Operation == arp.OperationRequest &&
-		p.SenderIP.Compare(vip) == 0 &&
-		bytes.Equal(p.TargetHardwareAddr, macAddrBroadcast) &&
-		p.TargetIP.Compare(vip) == 0
-}
-
 func WatchGARP(ctx context.Context, addr netip.Addr, callback func(*arp.Packet) error) error {
 	const typ = syscall.SOCK_RAW | syscall.SOCK_CLOEXEC
 	proto := int(htons(uint16(syscall.ETH_P_ARP)))
@@ -95,14 +64,45 @@ func WatchGARP(ctx context.Context, addr netip.Addr, callback func(*arp.Packet) 
 			}
 			return err
 		}
-		pkt, _, err := ParseARPPacket(buf[:n])
+		pkt, _, err := parseARPPacket(buf[:n])
 		if err != nil {
 			return err
 		}
-		if IsGARPPacket(pkt, addr) {
+		if isGARPPacket(pkt, addr) {
 			if err := callback(pkt); err != nil {
 				return err
 			}
 		}
 	}
+}
+
+var (
+	// errInvalidARPPacket is returned when an ethernet frame does not
+	// indicate that an ARP packet is contained in its payload.
+	errInvalidARPPacket = errors.New("invalid ARP packet")
+)
+
+func parseARPPacket(buf []byte) (*arp.Packet, *ethernet.Frame, error) {
+	f := new(ethernet.Frame)
+	if err := f.UnmarshalBinary(buf); err != nil {
+		return nil, nil, err
+	}
+
+	// Ignore frames which do not have ARP EtherType
+	if f.EtherType != ethernet.EtherTypeARP {
+		return nil, nil, errInvalidARPPacket
+	}
+
+	p := new(arp.Packet)
+	if err := p.UnmarshalBinary(f.Payload); err != nil {
+		return nil, nil, err
+	}
+	return p, f, nil
+}
+
+func isGARPPacket(p *arp.Packet, vip netip.Addr) bool {
+	return p.Operation == arp.OperationRequest &&
+		p.SenderIP.Compare(vip) == 0 &&
+		bytes.Equal(p.TargetHardwareAddr, macAddrBroadcast) &&
+		p.TargetIP.Compare(vip) == 0
 }
